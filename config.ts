ba460a5504301e2cd7f95af0c0456120b6aead4b
jross19 <45078297+jross19@users.noreply.github.com>
2019-09-08 21:19:58 -0400

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,12 @@
+export let config = {
+    "token" : "NTc1NDU3ODA5MzAzMzM5MDA4.XXWnfQ.UktlZukR94uCpybXqsRyP_K9Klw",
+    "prefix": "!",
+    "commands": [
+        "testCommand",
+        "butter",
+        "join",
+        "disconnect",
+        "jake"
+
+    ]
+}
