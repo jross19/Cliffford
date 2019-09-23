fbddfeeaf9d3466c7f5a8f1f05ec1accfee03c0c
jross19 <45078297+jross19@users.noreply.github.com>
2019-09-22 23:42:09 -0400

    Update config.js

--- dist/src/config.js
+++ dist/src/config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
-    "token": "NTc1NDU3ODA5MzAzMzM5MDA4.XNIPDA.Ac4CKUUz9dMEMtebCl2EmqzK9Gw",
+    "token": "",
     "prefix": "?"
 };
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFHO0lBQ2hCLE9BQU8sRUFBRyw2REFBNkQ7SUFDdkUsUUFBUSxFQUFFLEdBQUc7Q0FDaEIsQ0FBQSJ9
